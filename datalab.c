1493d2c541e2f8e1d5f0ea3ade44f84799f0fe76
王俊杰 <galileo233@163.com>
2019-04-11 21:20:36 +0800

    完成了bitXOR和Tmin

--- datalab.c
+++ datalab.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 //1
 /*
@@ -8,9 +9,9 @@
  *   Legal ops: ~ &
  *   Max ops: 14
  *   Rating: 1
+ *   解：~(~(x&~(x&y))&~(y&~(x&y))) ==>（若可用用或门）~x&y+x&~y
  */
 int bitXor(int x, int y) {
-//~(~(x&~(x&y))&~(y&~(x&y))) ==>（若可用用或门）~x&y+x&~y
     int t1 = ~(x&y);
     int t2 = x&t1;
     int t3 = ~t2;
@@ -25,15 +26,49 @@ int bitXor(int x, int y) {
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 4
  *   Rating: 1
+ *   解：tmin为1000···000,共32位，1个1，31个0；故将 000···01的1左移31位即可。
  */
-int tmin(void) {
-
-    return 2;
-
+int tmin() {
+    return 1<<31;
 }
-
+//2
+/*
+ * isTmax - returns 1 if x is the maximum, two's complement number,
+ *     and 0 otherwise
+ *   Legal ops: ! ~ & ^ | +
+ *   Max ops: 10
+ *   Rating: 1
+ */
+int isTmax(int x) {
+    return ~((x+1)+(x+1))&~(x+1);
+}
+/*
+ * allOddBits - return 1 if all odd-numbered bits in word set to 1
+ *   where bits are numbered from 0 (least significant) to 31 (most significant)
+ *   Examples allOddBits(0xFFFFFFFD) = 0, allOddBits(0xAAAAAAAA) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 2
+ */
+int allOddBits(int x) {
+    return 2;
+}
+/*
+ * negate - return -x
+ *   Example: negate(1) = -1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 2
+ */
+int negate(int x) {
+    return 2;
+}
+/**test**/
 int main() {
-    int t = bitXor(4,5);
-
+//    int t = bitXor(4,5);
+    int tmi = !((INT_MAX+1)+(INT_MAX+1));
+    int tmax = INT_MAX +1;
+    int tmin = INT_MIN;
+    int t= isTmax(tmax);
     return 0;
 }
