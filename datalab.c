edff9650edb675526e31add8a100e472ebe0b6b5
王俊杰 <galileo233@163.com>
2019-04-13 10:56:05 +0800

    isTmax解决，但是有个对！操作符的疑问

--- datalab.c
+++ datalab.c
@@ -38,9 +38,25 @@ int tmin() {
  *   Legal ops: ! ~ & ^ | +
  *   Max ops: 10
  *   Rating: 1
+ *   解：利用以下性质：
+ *   1、Tmax+1=Tmin,
+ *   2、Tmin+Tmin会溢出最高位，得到0；
+ *   3、再取逻辑非即可得到1.
+ *   但是有一个问题即，除了输入Tmax,输入-1也会最终返回1.因此需要区分它们。
+ *   它们的区别在于Tmax+1=Tmin，还需要Tmin+Tmin才转换为0，而-1+1直接等于0了。
+ *   因此再逻辑与上x + 1 != 0,输入-1输出为0，而输入Tmax输出为1.
  */
 int isTmax(int x) {
-    return ~((x+1)+(x+1))&~(x+1);
+    int a = x +1;
+    int b = a+a;
+    int c = !b;
+//    疑问：为何df,t1返回1，t2返回0？（似乎和优先级有关）
+//    int d = (x + 1) + (x + 1);
+//    int df = !d;
+//    int t1 =!(x+1+x+1);
+//    int t3 =!((x+1)+(x+1));
+    int t2 = x + 1 != 0;
+    return c&t2;
 }
 /*
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -67,7 +83,7 @@ int negate(int x) {
 int main() {
 //    int t = bitXor(4,5);
     int tmi = !((INT_MAX+1)+(INT_MAX+1));
-    int tmax = INT_MAX +1;
+    int tmax = INT_MAX;
     int tmin = INT_MIN;
     int t= isTmax(tmax);
     return 0;
